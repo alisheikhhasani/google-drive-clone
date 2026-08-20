16f5b03d4aab2e33d99adf6f8b83330907236173
Ali Sheikhhasani <ali.sheikhhasani1384@gmail.com>
2026-08-20 23:22:15 +0330

    WIP: Create register page

---
+++ resources/views/pages/⚡register.blade.php
@@ -0,0 +1,77 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component
+{
+    public string $name = '';
+    public string $email = '';
+    public string $password = '';
+    public string $password_confirmation = '';
+
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string'],
+            'email' => ['required', 'string', 'email', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed']
+        ];
+    }
+
+    public function render()
+    {
+        return $this->view()
+            ->layout('layouts::auth')
+            ->title('ثبت نام گوگل درایو');
+    }
+
+    public function registerUser()
+    {
+        $this->validate();
+    }
+};
+?>
+
+<div>
+    <form wire:submit.prevent="registerUser">
+        <fieldset class="fieldset">
+            <legend class="fieldset-legend">{{ __('validation.attributes.name') }}</legend>
+            <input type="text" class="input input-primary w-full" wire:model="name">
+            @error('name')
+            <p class="label text-error">
+                {{ $message }}
+            </p>
+            @enderror
+        </fieldset>
+
+        <fieldset class="fieldset">
+            <legend class="fieldset-legend">{{ __('validation.attributes.email') }}</legend>
+            <input type="text" class="input input-primary w-full" wire:model="email">
+            @error('email')
+            <p class="label text-error">
+                {{ $message }}
+            </p>
+            @enderror
+        </fieldset>
+
+        <fieldset class="fieldset">
+            <legend class="fieldset-legend">{{ __('validation.attributes.password') }}</legend>
+            <input type="text" class="input input-primary w-full" wire:model="password">
+            @error('password')
+            <p class="label text-error">
+                {{ $message }}
+            </p>
+            @enderror
+        </fieldset>
+
+        <fieldset class="fieldset">
+            <legend class="fieldset-legend">{{ __('validation.attributes.password_confirmation') }}</legend>
+            <input type="text" class="input input-primary w-full" wire:model="password_confirmation">
+        </fieldset>
+
+        <button class="btn btn-primary btn-block mt-6">
+            <span class="loading loading-spinner" wire:loading wire:target="registerUser"></span>
+            <span wire:loading.remove wire:target="registerUser">{{ __('forms.register.submit') }}</span>
+        </button>
+    </form>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::middleware('guest')->group(function () {
+    Route::livewire('/register', 'pages::register')->name('register');
+});
 
